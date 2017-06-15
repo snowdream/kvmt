a8124c4becbe0cff952874e97bc36aa270e80cbf
snowdream <yanghui1986527@gmail.com>
2017-06-15 18:25:04 +0800

    Add: support for koa-morgan

--- app.js
+++ app.js
@@ -3,16 +3,33 @@ const app = new Koa();
 const views = require('koa-views')(__dirname + '/views', {
     extension: 'pug'
 });
-const json = require('koa-json')();
+const env = require('get-env')();
+const json = require('koa-json')(({ pretty: env === 'dev', param: 'pretty' }));
 const responseTime = require('koa-response-time')();
 const onerror = require('koa-onerror');
 const bodyparser = require('koa-bodyparser')({
     enableTypes:['json', 'form', 'text']
 });
-const logger = require('koa-logger')();
+
+//logger
+const fs = require('fs');
+const path = require('path');
+const rfs = require('rotating-file-stream');
+const logDirectory = path.join(__dirname, 'log');
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+// create a rotating write stream
+const accessLogStream = rfs('access.log', {
+    size:     '10M', // rotate every 10 MegaBytes written
+    interval: '1d',  // rotate daily
+    compress: 'gzip', // compress rotated files
+    path: logDirectory
+});
+const logger = require('koa-morgan')('combined', {stream: accessLogStream});
+
 const koa_static = require('koa-static')(__dirname + '/public');
 const staticCache = require('koa-static-cache')((__dirname + '/public'), {
-    maxAge: 24 * 60 * 60  //Add these files to caches for a year
+    maxAge: 24 * 60 * 60
 });
 const convert_staticCache = require('koa-convert')(staticCache);
 
